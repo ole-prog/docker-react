adcc707cdf802cfccbd223cfcf36b8c9a7a54d3c
Oleg Dimitrov <o7leg@abv.bg>
2020-02-03 06:48:14 -0800

    change app text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          I was change.
+          A tova dostapvash li go ?
         </p>
       </div>
     );
